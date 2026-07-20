ed7ea8630186aeb14a11bd86300d562e90c903c6
sarahjo123 <jojeongin375@gmail.com>
2026-07-20 18:33:22 +0900

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,21 @@
+const questions = [
 
+{
+    question: "염소산칼륨은 몇 류인가?",
+    choices: ["제1류", "제2류", "제3류", "제4류"],
+    answer: 0
+},
+
+{
+    question: "황린은 몇 류인가?",
+    choices: ["제2류", "제3류", "제4류", "제5류"],
+    answer: 1
+},
+
+{
+    question: "적린은 몇 류인가?",
+    choices: ["제2류", "제3류", "제4류", "제5류"],
+    answer: 0
+}
+
+];
